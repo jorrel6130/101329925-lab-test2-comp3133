8371fdf468ec0b92793e092328dddc6c8e8df30e
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-02 13:25:13 -0400

    Get request for Space X API

--- src/app/network/spacexapi.service.ts
+++ src/app/network/spacexapi.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpacexapiService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getMissions(): Observable<any> {
+    return this.httpClient.get('https://api.spacexdata.com/v3/launches')
+  }
 }
