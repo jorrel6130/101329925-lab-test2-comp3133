2960b5db676f33c196adac8d1926edb3e57d7acf
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-02 13:08:30 -0400

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'missionlist',
+        pathMatch: 'full'
+    },
+    {
+        path: 'missionlist',
+        component: MissionlistComponent
+    },
+    {
+        path: 'missiondetails',
+        component: MissiondetailsComponent
+    },
+];
