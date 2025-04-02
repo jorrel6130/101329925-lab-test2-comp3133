e6507715e808d9e2a883591cb7faef22377e6013
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-02 16:53:00 -0400

    Testing solutions for failed Vercel deployment

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,17 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'missionlist',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'missiondetails/:flight_number',
+    renderMode: RenderMode.Server
   }
 ];
